829e0b35a36f1c8b072923251bc751759d7d990c
asikk <askarzurabekov@gmail.com>
2026-05-06 01:28:29 +0500

    Fix Docker build: skip artisan scripts during composer install

--- travelagency-laravel/render-start.sh
+++ travelagency-laravel/render-start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+echo "=== Discovering packages ==="
+php artisan package:discover --ansi
+
 echo "=== Caching config/routes/views ==="
 php artisan config:cache
 php artisan route:cache
